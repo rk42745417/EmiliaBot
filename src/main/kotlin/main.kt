729666809eedf20cd4d1427db11095eee767c5a1
rk42745417 <rk427454171@gmail.com>
2020-12-26 23:03:30 +0800

    Complete Owner's commands

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,12 @@
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.Permission
-import net.dv8tion.jda.api.entities.GuildChannel
-import net.dv8tion.jda.api.entities.PrivateChannel
-import net.dv8tion.jda.api.entities.SelfUser
-import net.dv8tion.jda.api.entities.TextChannel
+import net.dv8tion.jda.api.entities.*
 import net.dv8tion.jda.api.events.ReadyEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.exceptions.InsufficientPermissionException
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.awt.Color
 
 fun main(args: Array<String>) {
     val builder = JDABuilder.createDefault(args[0])
@@ -31,11 +30,56 @@ class Commands : ListenerAdapter() {
             return
         command = command.substring(0, command.length - 1)
 
+        if(message.author.idLong == 303139670508830730L) {
+            ownerCommand(command, event, content, message)
+        }
+
         if(command.matches(Regex("[0-9]{6}"))) {
             HentaiConverter(command, event.textChannel, message).proc()
         }
         if(command == "pixiv" && content.size > 1 && content[1].isNotEmpty()) {
             PixivConverter(content[1], event.textChannel).proc()
+        }
+    }
+    private fun ownerCommand(command: String, event: MessageReceivedEvent, content: List<String>, message: Message) {
+        if(content.size == 1)
+            return
+        if(command == "avatar") {
+            event.jda.retrieveUserById(content[1]).queue { user ->
+                if (user != null) {
+                    println(user.name)
+                    message.channel.sendMessage(EmbedBuilder()
+                            .setColor(Color.CYAN)
+                            .setTitle("Avatar of")
+                            .setDescription("<@!${user.id}>")
+                            .setImage(user.avatarUrl)
+                            .build()
+                    ).queue()
+                }
+            }
+
+        }
+        if(command == "say") {
+            message.channel.sendMessage(content.subList(1, content.size).joinToString("")).queue()
+        }
+        if(command == "remove") {
+            val msg = message.channel.history.getMessageById(content[1])
+            message.channel.deleteMessageById(content[1]).queue()
+            message.delete().queue()
+        }
+        if(command == "removes") {
+            try {
+                val times = content[1].toInt()
+                if(times <= 10) {
+                    message.channel.history.retrievePast(times + 1).queue { messages ->
+                        messages.forEach {message ->
+                            message.delete().queue()
+                        }
+                    }
+                }
+            } catch(e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
